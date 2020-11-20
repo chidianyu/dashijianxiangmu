87795b031b2e03f4a9d8519496f90f17ae223ea9
hello word <3504957178@qq.com>
2020-11-20 22:18:58 +0800

    完成了注册界面

---
+++ index.js
@@ -0,0 +1,67 @@
+$(function () {
+
+    $("#link_reg").on("click", function () {
+        $('.login-box').hide();
+        $('.reg-box').show();
+
+    })
+    $("#link_login").on("click", function () {
+        $('.login-box').show();
+        $('.reg-box').hide()
+
+    })
+
+    let form = layui.form
+
+    form.verify({
+
+        passs: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+        repwd: function (value) {
+
+            let pass = $('.reg-box [name=password]').val()
+            if (pass !== value) {
+                return '两次密码不一致'
+            }
+
+        }
+
+    });
+    // 监听注册表单的提交事件
+    $('#form_reg').on('submit', function (e) {
+        // 1. 阻止默认的提交行为
+        e.preventDefault()
+        let data = {
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password]').val(),
+        }
+        $.post('http://ajax.frontend.itheima.net/api/reguser', data, function (res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message)
+            }
+
+            layer.msg('注册成功，请登录！')
+        })
+    })
+
+    let layer = layui.layer
+    // var layer = layui.layer
+    $('#form_login').on('submit', function (e) {
+        e.preventDefault()
+        $.ajax({
+            url: 'http://ajax.frontend.itheima.net/api/login',
+            method: 'POST',
+            data: $(this).serialize(),
+            success: function (res) {
+                // console.log(res);
+                if (res.status !== 0) {
+                    return layer.msg('登陆失败')
+                }
+                layer.msg('注册成功，请登录！')
+                localStorage.setItem('token', res.token)
+                location.href = '/index.html'
+
+            }
+        })
+
+    })
+})
